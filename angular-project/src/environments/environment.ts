8681b0bbbf140bf8192f7313197373dc9dea555f
ruchiisinghal <101795766+ruchiisinghal@users.noreply.github.com>
2022-12-08 18:48:37 +0530

    basic angular/firebase

---
+++ angular-project/src/environments/environment.ts
@@ -0,0 +1,19 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'crud-student123',
+    appId: '1:62981051822:web:9fdacda62db1ec8c51a43f',
+    storageBucket: 'crud-student123.appspot.com',
+    apiKey: 'AIzaSyD20Y5sxVcuHBply8SbnsdR7VpD43GdB_0',
+    authDomain: 'crud-student123.firebaseapp.com',
+    messagingSenderId: '62981051822',
+  }
+};
+
+
+
